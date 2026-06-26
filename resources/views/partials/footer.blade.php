23b03cefaa03276878d83afd69b6d94eec806572
aryopratamaa <mr.aryopratama@gmail.com>
2026-06-26 10:44:24 +0700

    5

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer py-4 border-top">
         <div class="container-fluid">
             <div class="py-6 px-6 text-center">
-                <p class="mb-0 fs-4">Design and Developed by | Aryo Pratama</p>
+                <p class="mb-0 fs-4">Design and Developed by Aryo Pratama</p>
             </div>
         </div>
     </div>
